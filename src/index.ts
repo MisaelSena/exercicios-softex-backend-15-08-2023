54ec33f871b00bedeb05222fcf8dec2a4c4304dc
Misael Sena <misaellsena@gmail.com>
2023-08-17 18:17:07 -0300

    Criada Função que soma os elementos de dois arrays de mesma dimensão

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { imprimeTabelaMultiplicacao, imprimeUmACinquentaSoftex, sequenciaDeColla
 import { calculadora, verficaImparOuPar, verificaAnoBissexto, verificaPalindromo, verificaPositivoENegativo } from "./controlesCondicionais";
 import { fibonacciAteDecimoTermo, imprimeDeZeroAVinte, imprimeUmADez, multiplosDeTres, produtoDeUmACinco, somaDeUmACem } from "./loops";
 import { celsiusParaFahrenheit, fatorial, inverteString, maiorValor, mediaArray, removeDuplicadosDoArray } from "./outrosConceitosBasicos";
-import { arrayOrdAlfabetica, arrayOrdCrescente, mairoValorArrayNumeros } from "./trabalhandoComArrays";
+import { arrayOrdAlfabetica, arrayOrdCrescente, mairoValorArrayNumeros, somaDoisArrays } from "./trabalhandoComArrays";
 import { contaLetraNaString, contaVogais, converteStringEmMaiuscula, removeEspacos } from "./trabalhandoComStrings";
 
 //Laços de Repetição (Loops):
@@ -65,3 +65,5 @@ arrayOrdCrescente();
 mairoValorArrayNumeros();
 
 arrayOrdAlfabetica();
+
+somaDoisArrays();
--- src/trabalhandoComArrays.ts
+++ src/trabalhandoComArrays.ts
@@ -21,4 +21,19 @@ export function arrayOrdAlfabetica() {
     console.log("Array Nomes não ordenados: [" + nomes+"]");
     console.log("Array Nomes em ordem alfabética: [" + nomes.sort()+"]");
   }
+//Crie um programa que some os elementos de dois arrays de mesma dimensão, criando um terceiro array com os resultados.
+export function somaDoisArrays() {
+    let lista1 = [1,2,3];
+    let lista2 = [4,5,6];
+    let soma = 0;
+    let somaElementosArray = [];
+
+    for (let i = 0; i < lista1.length; i++) {
+        soma = lista1[i]+lista2[i];
+        somaElementosArray.push(soma);        
+    }
+    console.log(`Array 1:[${lista1}]`);
+    console.log(`Array 2:[${lista2}]`);
+    console.log(`Elementos de mesma posição dos Arrays 1 e 2 somados:[${somaElementosArray}]`);
+}
 
